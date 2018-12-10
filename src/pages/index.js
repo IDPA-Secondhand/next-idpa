3d7920d75e0fb7573eabad753f4dbc35e2bb3739
[thecubix] <[timon.forrer@gmail.com]>
2018-12-10 18:38:53 +0100

    Added hero section to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
 import Card from '../components/card'
+import { ReactComponent as OnlineShopping } from '../images/online-shopping.svg'
 
 class IndexPage extends React.Component {
   render () {
@@ -15,29 +16,52 @@ class IndexPage extends React.Component {
 
     return (
       <Layout>
+        <>
+        <section className="hero is-large is-primary">
+          <div className="sh-container hero-text column is-10-tablet is-offset-1-tablet is-8-desktop is-offset-2-desktop is-6-fullhd is-offset-3-fullhd">
+            <h1 className="h1">Mach den<br/>Unterschied.</h1>
+            <hr className="seperator" />
+            <div className="columns">
+              <div className="column is-5">
+                <p className="extended">
+                  Hilf uns im Kampf gegen die Wegwerfgesellschaft und kaufe bereits Gebrauchtes.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <OnlineShopping className="hero-icon" />
+
+        </section>
+
         <div className="sh-container">
           <div className="column is-10-tablet is-offset-1-tablet is-8-desktop is-offset-2-desktop is-6-fullhd is-offset-3-fullhd">
 
-            <div className="columns is-multiline" style={{marginTop: '3em'}}>
-              {shops.map((shop) => {
-                let shopBody = shop.data
-                const categories = shopBody.productRange.map(e => e.data)
-                const imageUrl = shopBody.fileNode[0].thumbnails.large.url
-                // console.log(imageUrl)
-                return(
-                  <div className="column is-half" key={shopBody.slug}>
-                    <Card
-                      title={shopBody.name}
-                      imageUrl={imageUrl}
-                      to={shopBody.slug}
-                      priceRange={Number(shopBody.priceRange[0].data.value)}
-                      categories={categories} />
-                  </div>
-                )
-              })}
-            </div>
+            <section className="section is-paddingless has-margin-large">
+
+              <h2 className="h2">Secondhand Läden in Aarau.</h2>
+              <div className="columns is-multiline has-margin">
+                {shops.map((shop) => {
+                  let shopBody = shop.data
+                  const categories = shopBody.productRange.map(e => e.data)
+                  const imageUrl = shopBody.fileNode[0].thumbnails.large.url
+                  // console.log(imageUrl)
+                  return(
+                    <div className="column is-half" key={shopBody.slug}>
+                      <Card
+                        title={shopBody.name}
+                        imageUrl={imageUrl}
+                        to={shopBody.slug}
+                        priceRange={Number(shopBody.priceRange[0].data.value)}
+                        categories={categories} />
+                    </div>
+                  )
+                })}
+              </div>
+            </section>
           </div>
         </div>
+        </>
       </Layout>
     )
   }
