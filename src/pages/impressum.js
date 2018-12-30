84124f7611877409f61eeef1ebbbde32b0ae53be
[thecubix] <[timon.forrer@gmail.com]>
2018-12-30 15:54:15 +0100

    Added impressum

---
+++ src/pages/impressum.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Layout from '../components/layout';
+
+const Impressum = () => (
+  <Layout>
+    <div className="column is-12-mobile is-10-tablet is-offset-1-tablet is-8-widescreen is-offset-2-widescreen is-6-fullhd is-offset-3-fullhd">
+      <div className="box has-margin">
+        <div class="impressum">
+          <h1 className="h1 has-margin">Impressum</h1>
+          <h2 className="h2 has-margin">Kontaktadresse</h2>
+          <p>Secondhando</p>
+          <p>Bahnhofstrasse 46</p>
+          <p>5000 Aarau</p>
+          <p>Schweiz</p>
+          <p>timon.forrer@gmail.com</p>
+          <p>&nbsp;</p>
+          <h2 className="h2 has-margin">Vertretungsberechtigte Personen</h2>
+          <p>
+            Andrea Huber<br />
+            Simon Freiermuth<br />
+            Tim König<br />
+            Timon Forrer
+          </p>
+          <p>&nbsp;</p>
+          <h2 className="h2 has-margin">Haftungsausschluss</h2>
+          <p>Der Autor übernimmt keinerlei Gewähr hinsichtlich der  inhaltlichen Richtigkeit, Genauigkeit, Aktualität, Zuverlässigkeit und  Vollständigkeit der Informationen.</p>
+          <p>Haftungsansprüche gegen den Autor wegen Schäden materieller  oder immaterieller Art, welche aus dem Zugriff oder der Nutzung bzw.  Nichtnutzung der veröffentlichten Informationen, durch Missbrauch der  Verbindung oder durch technische Störungen entstanden sind, werden  ausgeschlossen.</p>
+          <p>Alle  Angebote sind unverbindlich. Der Autor behält es sich ausdrücklich vor, Teile  der Seiten oder das gesamte Angebot ohne gesonderte Ankündigung zu verändern,  zu ergänzen, zu löschen oder die Veröffentlichung zeitweise oder endgültig  einzustellen.</p>
+          <p>&nbsp;</p>
+          <h2 className="h2 has-margin">Haftung für Links</h2>
+          <p>Verweise und Links auf Webseiten  Dritter liegen ausserhalb unseres Verantwortungsbereichs Es wird jegliche Verantwortung für solche Webseiten  abgelehnt.  Der Zugriff und die Nutzung solcher Webseiten erfolgen  auf eigene Gefahr des Nutzers oder der Nutzerin. </p>
+          <p>&nbsp;</p>
+          <h2 className="h2 has-margin">Urheberrechte</h2>
+          <p>Die Urheber- und alle anderen Rechte an Inhalten, Bildern, Fotos oder anderen Dateien auf der Website gehören ausschliesslich <strong>der Firma Secondhando</strong> oder den speziell genannten  Rechtsinhabern. Für die Reproduktion jeglicher Elemente ist die schriftliche Zustimmung der Urheberrechtsträger im Voraus einzuholen.</p>
+          <p>&nbsp;</p>
+          <h2 className="h2 has-margin">Quelle</h2>
+          <p> Dieses Impressum wurde am <strong>30.12.2018</strong> mit dem  Impressum-Generator <strong><a href="http://www.bag.ch/impressum-generator">http://www.bag.ch/impressum-generator</a></strong> der Firma Brunner Medien AG in Kriens erstellt. Die Brunner Medien AG in Kriens übernimmt keine  Haftung. </p>
+          <p style={{ visibility: 'hidden' }}></p>
+        </div>             
+      </div>
+      </div>
+  </Layout>
+)
+
+export default Impressum
