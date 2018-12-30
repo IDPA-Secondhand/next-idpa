566dedb05d7b073c13b70032a36f73f95200cc83
[thecubix] <[timon.forrer@gmail.com]>
2018-12-30 17:25:41 +0100

    Added links

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,24 +6,26 @@ const header = () => {
     <footer className="footer">
       <div className="content">
         <div className="columns">
-          <div className="column is-10-tablet is-offset-1-tablet is-8-desktop is-offset-2-desktop is-6-fullhd is-offset-3-fullhd">
-            <div className="columns is-multiline">
-                <div className="column">
-                  <h2 className="h2">Seiten</h2>
-                  <Link to="/">Home</Link><br />
-                  <Link to="/overview/">Übersicht</Link>
-                </div>
-                <div className="column">
-                  <h2 className="h2">Weitere Infos</h2>
-                  <Link to="/about-us/">Über uns</Link><br />
-                  <Link to="/impressum/">Impressum</Link><br />
-                </div>
-                <div className="column is-12">
-                  <span>&copy; 2018 Andrea Huber, Simon Freiermuth, Tim König, Timon Forrer</span>
-                </div>
+        <div className="column is-12-mobile is-10-tablet is-offset-1-tablet is-8-widescreen is-offset-2-widescreen is-6-fullhd is-offset-3-fullhd">
+          <div className="columns is-multiline">
+              <div className="column">
+                <h2 className="h2">Seiten</h2>
+                <Link to="/">Home</Link><br />
+                <Link to="/overview/">Übersicht</Link><br />
+                <Link to="/categories/">Kategorien</Link><br />
+                <Link to="/new-shop/">Ihren Shop eintragen</Link>
+              </div>
+              <div className="column">
+                <h2 className="h2">Weitere Infos</h2>
+                <Link to="/about-us/">Über uns</Link><br />
+                <Link to="/impressum/">Impressum</Link><br />
+              </div>
+              <div className="column is-12">
+                <span>&copy; 2018 Andrea Huber, Simon Freiermuth, Tim König, Timon Forrer</span>
               </div>
             </div>
           </div>
+        </div>
       </div>
     </footer>
   )
