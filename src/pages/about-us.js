76f13601bba87530c76dfb4cf4c670d966ba7848
[thecubix] <[timon.forrer@gmail.com]>
2018-12-30 17:25:58 +0100

    Updated layout a bit

---
+++ src/pages/about-us.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import Layout from '../components/layout';
+
+
+const AboutUs = () => (
+  <Layout>
+    <div className="column is-12-mobile is-10-tablet is-offset-1-tablet is-8-widescreen is-offset-2-widescreen is-6-fullhd is-offset-3-fullhd">
+      <div className="box sheet has-margin">
+      <h1 className="h1 has-margin">Über uns</h1>
+      <h2 className="h3 has-margin">Einleitung</h2>
+      <p className="has-margin-large">
+        Wir sind vier Mediamatiker, die im Rahmen einer sog. interdisziplinären Projektarbeit (IDPA)
+        diese Website erstellten. «Wegwerfgesellschaft» lautete das Oberthema für unsere Arbeit, an der wir
+        zwischen August 2018 und März 2019 arbeiteten. Zu diesem Oberthema konnten wir mit wenigen
+        Einschränkungen ein Unterthema aussuchen, welches uns interessiert.
+      </p>
+      <h2 className="h3 has-margin">Themenwahl</h2>
+      <p className="has-margin-large">
+        Unsere Gruppe entschied sich einstimmig für das Thema «Second Hand Läden», da diese unserer Ansicht
+        nach ein idealer Gegentrend zur Wegwerfgesellschaft darstellen. Sie sind eine ideale Plattform, um
+        den Lebenszyklus eines Artikels zu verlängern, womit weniger Produkte entsorgt werden müssen und
+        somit der Bedarf an neuen Produkten reduziert werden kann. Allerdings wird das Potenzial dieser
+        Plattform noch nicht genug ausgenutzt.
+      </p>
+      <p className="has-margin-large">
+        Second Hand Läden erschienen uns deshalb aus diversen Gründen ein passendes Thema zu sein.
+      </p>
+      <p className="has-margin-large">
+        Einerseits boten sie grosses Potenzial für eine Feldarbeit, da wir anhand von Umfragen mit
+        möglichen Kunden genau erkennen konnten, welche Aspekte von Second Hand Läden bereits genutzt
+        werden und welche noch nicht.
+      </p>
+      <p className="has-margin-large">
+        Andererseits war diese Thematik auch für unsere Gruppe eine komplett neue. Als wir auf die
+        Themenwahl kamen, stellten wir sofort fest, wie viel Potenzial Second Hand Läden alleine bei
+        uns noch hätten.
+      </p>
+      <h2 className="h3 has-margin">Zur Website</h2>
+      <p className="has-margin-large">
+        Passend zu unserer Ausbildung als Mediamatiker wollten wir deshalb den digitalen Aspekt in das
+        Thema Second Hand Läden einbringen, in dem wir eine Website erstellen, auf der alle Second Hand
+        Läden in der Region Aarau, inklusive nötiger Informationen, aufgeführt sind. Somit konnten wir uns
+        noch aktiver selber in die Thematik einbringen.
+      </p>
+      <p className="has-margin-large">
+        Das Ziel dieser Arbeit war es, aufzuzeigen, wie das Potenzial von Second Hand Läden weiter
+        ausgeschöpft werden kann, indem die Ansprüche der möglichen und der bestehenden Kunden aktiv
+        in ein Produkt einfliessen, welches die Nutzung von Second Hand Läden fördert und vereinfacht.
+        Dieses Produkt wird anschliessend in Form einer Vermittlungswebsite von uns erstellt mit dem Ziel,
+        dass es auch genutzt werden kann.
+      </p>
+      <p className="has-margin-large">
+        Die Grundlage für den Aufbau der Website ist eine Umfrage, mit der wir herausgefunden haben, auf
+        welche Aspekte mögliche und bestehende Kunden Wert legen. Die Fragen bezogen sich nicht nur auf die
+        Tiefe der Informationen, die auf der Website verfügbar sind, sondern auch auf die Nutzung von
+        Second Hand Läden generell, wodurch wir ein besseres Verständnis von diesen im Allgemeinen
+        erhielten. Auch diese Informationen sollten auf der Website zur Verfügung gestellt werden.
+      </p>
+      <p className="has-margin-large">
+        Für uns ist auch wichtig, dass nicht nur die Erkenntnisse aus der Umfrage, sondern auch unser
+        technisches Wissen aus mehr als drei Jahren Mediamatiker Lehre mit einfliessen. Die ganze Website
+        soll mithilfe eines Frameworks erstellt werden, welches es erlaubt, die Website praktisch einsetzen
+        zu können.
+      </p>
+      <h2 className="h3 has-margin">Danksagung</h2>
+      <p className="has-margin-large">
+        Wir bedanken uns auch vielmals bei unserem Hauptexperten Peter Hauri, der uns stets hilfsbereit
+        beraten hat und bei unserer Co-Expertin Denise Bussinger, die uns beibrachte, wie eine solche
+        Arbeit korrekt angegangen wird.
+      </p>
+      <p className="has-margin-large">
+        Ebenfalls bedanken wir uns bei den zahlreichen Personen, die unsere Online-Umfrage ausgefüllt haben
+        und somit aktiv den Aufbau unseres Produkts beeinflusst haben.
+      </p>
+      </div>
+    </div>
+  </Layout>
+)
+
+export default AboutUs
