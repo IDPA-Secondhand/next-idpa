57eedda6d487199d03072477ff50a370da64b5a8
[thecubix] <[timon.forrer@gmail.com]>
2018-12-30 17:26:06 +0100

    Added categories page

---
+++ src/pages/categories.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../components/layout';
+
+class Categories extends React.Component {
+  render () {
+    return (
+      <Layout>
+        <div className="column has-margin is-12-mobile is-10-tablet is-offset-1-tablet is-8-widescreen is-offset-2-widescreen is-6-fullhd is-offset-3-fullhd">
+          <h1 className="h1 has-margin">Kategorien</h1>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default Categories
