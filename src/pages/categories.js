6698841d447e04cbef9b3c8d7ae25a654e3cbc86
v0ll0v <46189508+v0ll0v@users.noreply.github.com>
2019-01-02 17:34:36 +0100

    test categories

--- src/pages/categories.js
+++ src/pages/categories.js
@@ -8,6 +8,25 @@ class Categories extends React.Component {
         <div className="column has-margin is-12-mobile is-10-tablet is-offset-1-tablet is-8-widescreen is-offset-2-widescreen is-6-fullhd is-offset-3-fullhd">
           <h1 className="h1 has-margin">Kategorien</h1>
         </div>
+      
+      
+      <div className="columns has-margin">
+                {shops.map((shop) => {
+                  let shopBody = shop.data
+                  const categories = shopBody.productRange.map(e => e.data)
+
+                  return(
+				  
+				  
+				  
+                    <div className="column is-half" key={shopBody.slug}>
+                      <Card
+                        categories={categories} />
+                    </div>
+                  )
+                })
+				}
+              </div>
       </Layout>
     )
   }
